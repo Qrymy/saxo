f13f59d633ef3196a55b6a39a5600b2abc145a03
Qrymy <regist@qry.mx>
2021-01-11 11:58:16 +0900

    Init region

--- packages/server/src/cat/index.ts
+++ packages/server/src/cat/index.ts
@@ -1,8 +1,11 @@
 import functions from 'firebase-functions'
 import { getColor } from '@shared/utils/getColor'
+import { REGIONS } from '@shared/const/Regions'
 
-export const getCat = functions.https.onRequest(async (_req, res) => {
-  const color = getColor()
+export const getCat = functions
+  .region(REGIONS.FUNCTIONS)
+  .https.onRequest(async (_req, res) => {
+    const color = getColor()
 
-  res.send(`I'm a ${color} cat.`)
-})
+    res.send(`I'm a ${color} cat.`)
+  })
--- packages/server/src/dog/index.ts
+++ packages/server/src/dog/index.ts
@@ -1,8 +1,11 @@
 import functions from 'firebase-functions'
 import { getColor } from '@shared/utils/getColor'
+import { REGIONS } from '@shared/const/Regions'
 
-export const getDog = functions.https.onRequest(async (_req, res) => {
-  const color = getColor()
+export const getDog = functions
+  .region(REGIONS.FUNCTIONS)
+  .https.onRequest(async (_req, res) => {
+    const color = getColor()
 
-  res.send(`I'm a ${color} dog.`)
-})
+    res.send(`I'm a ${color} dog.`)
+  })
