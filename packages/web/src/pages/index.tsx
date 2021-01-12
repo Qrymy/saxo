759394cf44019a5bdf81f054cd1c6e508ad24b38
Qrymy <regist@qry.mx>
2021-01-12 09:41:31 +0900

    Init web directory

---
+++ packages/web/src/pages/index.tsx
@@ -0,0 +1,13 @@
+import { useMemo } from 'react'
+import { NextPage } from 'next'
+import { getColor } from '@saxo/shared/src/utils/getColor'
+
+const IndexPage: NextPage = () => {
+  const color = useMemo(() => {
+    return getColor()
+  }, [getColor])
+
+  return <div>{color}</div>
+}
+
+export default IndexPage
